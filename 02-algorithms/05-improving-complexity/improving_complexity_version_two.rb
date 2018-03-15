74e7a193bdba2b1df5cf123e4f00d9e3109d66a7
zack <zack.etc@gmail.com>
2018-03-14 18:36:13 -0700

    improving complexity done

--- 02-algorithms/05-improving-complexity/improving_complexity_version_two.rb
+++ 02-algorithms/05-improving-complexity/improving_complexity_version_two.rb
@@ -1,24 +1,5 @@
- def poorly_written_ruby(*arrays)
-   sorted_array = []
-   arrays.each do |array|
-     array.sort!
-     array.each do |value|
-      if sorted_array.length < 1 
-        sorted_array << value
-      else
-        i = 0 
-        while i < sorted_array.length
-           if value <= sorted_array[i]
-             sorted_array.insert(i, value)
-             break
-           elsif i == sorted_array.length - 1
-             sorted_array << value
-             break
-           end
-          i+=1
-        end
-      end
-     end
-   end
-   sorted_array
- end
+require_relative 'merge_sort'
+ 
+def poorly_written_ruby(*arrays)
+    merge_sort(arrays.flatten)
+end
